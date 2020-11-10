d0450c46756e3073898eb39baac9d6e2391ac0e7
Joel Berryere (jtb648) <jtb648@mail.usask.ca>
2020-11-10 15:30:36 -0600

    Privatized instance variables, added a started instance variable to check if the timer should be stopped or started and changed the display to work better.

--- Timer.py
+++ Timer.py
@@ -17,63 +17,64 @@ class Timer(object):
         :param allotted_time: The initial time given to a Player to make their moves.
         :param: enabled: Determines if the timer is enabled or not
         """
-        self.time_remaining = time_remaining
-        self.allotted_time = allotted_time  # for 5 minutes per Player? should this be combined with time_remaining
-        self.enabled = enabled  # is this for starting the timer?
+        self.__time_remaining = time_remaining
+        self.__allotted_time = allotted_time  # for 5 minutes per Player? should this be combined with time_remaining
+        self.__enabled = enabled  # is this for starting the timer?
+        self.__started = False
 
     def time_out(self):
+        """Notifies the Player that they have run out of time and stops the game?"""
+        print("donezo")  # placeholder
         """TODO"""
 
     def warn_of_little_time(self):
         """Warns a Player if their time remaining is low"""
-        if self.time_remaining <= 30:
+        if self.__time_remaining <= 30:
             print("Uh oh")
 
     def display(self):
         """Displays the time remaining for the Player to play their moves"""
-        if self.enabled:
-            start_time = time.time()
-            seconds = 0
-            while seconds < self.time_remaining:                      # Problem will be when paused, how to
-                time.sleep(1)                                         # pause time.time() and will time.sleep
-                print(self.allotted_time - time.time() - start_time)  # stop everything else
-                Timer.set_time_remaining(self, self.time_remaining - 1)
-                seconds += 1
-        """TODO"""
+        if self.__enabled and self.__started:
+            while self.__allotted_time >= self.__time_remaining:
+                minutes, seconds = divmod(self.__time_remaining, 60)
+                timer = '{:2d}:{:02d}'.format(minutes, seconds)
+                print(timer)
+                time.sleep(1)
+                self.__time_remaining -= 1
+            # figure out how to stop it mid count
 
     def start(self):
         """Starts the time remaining for a Player"""
-        # Should instead put get/set_enabled into the start and stop and have them determine enabled?
-        """TODO"""
+        self.__started = True
 
     def stop(self):
         """Stops the time remaining for a Player"""
-        # Should instead put get/set_enabled into the start and stop and have them determine enabled?
-        """TODO"""
+        self.__started = False
 
     def get_time_remaining(self):
         """Returns the time remaining for a Player"""
-        return self.time_remaining
+        return self.__time_remaining
 
     def set_time_remaining(self, time_remaining):
         """Sets the time remaining for a Player"""
-        self.time_remaining = time_remaining
+        self.__time_remaining = time_remaining
 
     def get_allotted_time(self):
         """Returns the allotted time for a Player"""
-        return self.allotted_time
+        return self.__allotted_time
 
     def set_allotted_time(self, allotted_time):
         """Sets the allotted time for a Player"""
-        self.allotted_time = allotted_time
+        self.__allotted_time = allotted_time
 
     def get_enabled(self):
         """Returns a boolean value for the timer if it is to be started or stopped"""
-        return self.enabled
+        return self.__enabled
 
     def set_enabled(self, enabled):
         """Sets a boolean value to start or stop the timer"""
-        self.enabled = enabled
+        self.__enabled = enabled
+
 
 def test_timer():
     t = Timer(300, 300, False)
