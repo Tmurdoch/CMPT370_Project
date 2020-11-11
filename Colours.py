bd912e43b2f14ef6fcf7c52db55a5da97b3909ec
Thomas Murdoch (tjm149) <tjm149@mail.usask.ca>
2020-11-11 12:42:50 -0600

    Resolve "Start Game Class (Blue Coloured Items)"

--- Colours.py
+++ Colours.py
@@ -1,11 +1,11 @@
-from enum import Enum
+from enum import IntEnum
 
-class Colour_Codes(Enum):
+class Colour_Codes(IntEnum):
     WHITE_BLACK = 0
     RED_BLACK = 1
 
 
-class Colour_Offset(Enum):
+class Colour_Offset(IntEnum):
     OFFSET_LIGHT = 0
     OFFSET_DARK = 1
 
