4a6ff0b51db9a34ef279e59c1200de24b41970ac
Michael Luciuk <mrl280@mail.usask.ca>
2020-11-12 21:51:27 -0600

    Implemented the build_list_of_moves() method to build the list of valid moves for a checkers move, does not allow for more than one jump.
    Broke the tests for possible moves because the class now takes in different parameters.

--- PossibleMoves.py
+++ PossibleMoves.py
@@ -9,12 +9,12 @@ from Game import Game
 from Board import Board
 
 
-class PossibleMoves():
+class PossibleMoves:
     """
     The class defining possible moves for a piece
     Attributes:
     __Piece: piece object wishing to move, moves to be calculated
-    __move: List of end locations possible to move to
+    __moves: List of end locations possible to move to
     __game: game object to get player locations, etc
     __board: board object, created during initialization
     """
@@ -22,11 +22,11 @@ class PossibleMoves():
     def __init__(self, game_square, game_obj):
         self.__game_square = game_square
         self.__Piece = game_square.get_piece()
-        self.__move = []
+        self.__moves = []
         self.__game = game_obj
         self.__row = game_square.get_row()
         self.__col = game_square.get_col()
-        self.__game_type = game_obj.get_game_type() #will come back as either chess or checkers
+        self.__game_type = game_obj.get_game_type()  # will come back as either "chess" or "checkers"
         self.__board = self.__game.get_board()
 
     def build_list_of_moves(self):
@@ -34,42 +34,98 @@ class PossibleMoves():
         Determine based on the piece where it can potentially move
         :return: 0 on success, -1 on failure
         """
-        if self.game_type == "checkers":
-            #generate possible moves for checkers
+        if self.__game_type.lower() == "checkers":
+            list_of_candidate_game_squares = []
+            # Generate possible moves for checkers
             if 0 < self.__row-1 < self.__board.get_size()-1 or 0 < self.__col-1 < self.__board.get_size()-1:
-                top_left =  self.__board.get_game_square(self.__row-1, self.__col-1)
-            if self.__row+1 > self.__board.get_size()-1 or self.__col+1 > self.__board.get_size()-1:
-                top_left =  self.__board.get_game_square(self.__row-1, self.__col-1)
+                top_left = self.__board.get_game_square(self.__row-1, self.__col-1)
+                list_of_candidate_game_squares.append(top_left)
+            if 0 < self.__row-1 < self.__board.get_size()-1 or 0 < self.__col+1 < self.__board.get_size()-1:
+                top_right = self.__board.get_game_square(self.__row-1, self.__col+1)
+                list_of_candidate_game_squares.append(top_right)
+
             if self.__Piece.get_promotion_status():
-                #is promoted
-                #check bounds of array
-                if 0 < self.__row-1 < self.__board.get_size()-1 or 0 < self.__col-1 < self.__board.get_size()-1:
-                    top_left =  self.__board.get_game_square(self.__row-1, self.__col-1)
-                if self.__row-1 > self.__board.get_size()-1 or self.__col-1 > self.__board.get_size()-1:
-                    top_left =  self.__board.get_game_square(self.__row-1, self.__col-1)
-                top_right = self.__board.get_game_square(self.__row-1, self.__col+1)
-                
-            else:
+                # Promoted checkers coins can also move backwards
+                if 0 < self.__row+1 < self.__board.get_size()-1 or 0 < self.__col-1 < self.__board.get_size()-1:
+                    bot_left = self.__board.get_game_square(self.__row+1, self.__col-1)
+                    list_of_candidate_game_squares.append(bot_left)
+                if 0 < self.__row+1 < self.__board.get_size()-1 or 0 < self.__col+1 < self.__board.get_size()-1:
+                    bot_right = self.__board.get_game_square(self.__row+1, self.__col+1)
+                    list_of_candidate_game_squares.append(bot_right)
 
-                #is not promoted  
+            for square in list_of_candidate_game_squares:
+                dest_piece = square.get_occupying_piece()
+                if dest_piece is None:
+                    continue
+                    # Do nothing, the square is a valid move
+                else:
+                    # There is a piece there, compare the colour
+                    if self.__game_square().get_occupying_piece().get_colour() == dest_piece.get_colour():
+                        # Pieces are the same colour, cannot make this move
+                        list_of_candidate_game_squares.remove(square)
+                    else:
+                        # Piece is an opponent piece, we need to go look at what is on the other side of it.
+                        if self.__game_square().get_col() < square.get_col():
+                            # We are to the right
+                            if self.__game_square().get_row() < square.get_row():
+                                # Bottom right
+                                if 0 < self.__row + 2 < self.__board.get_size() - 1 or 0 < self.__col + 2 < self.__board.get_size() - 1:
+                                    new_bot_right = self.__board.get_game_square(self.__row + 2, self.__col + 2)
+                                    if new_bot_right.get_occupying_piece() is None:
+                                        list_of_candidate_game_squares.append(new_bot_right)
+                                        # TODO: Right now this only allows for one jump, need to check to see if
+                                        #  there are multi-jump moves available
+                            else:
+                                # Top right
+                                if 0 < self.__row - 2 < self.__board.get_size() - 1 or 0 < self.__col + 2 < self.__board.get_size() - 1:
+                                    new_top_right = self.__board.get_game_square(self.__row - 2, self.__col + 2)
+                                    if new_top_right.get_occupying_piece() is None:
+                                        list_of_candidate_game_squares.append(new_top_right)
+                                        # TODO: Right now this only allows for one jump, need to check to see if
+                                        #  there are multi-jump moves available
+                        else:
+                            # Destination square we are comparing to is to the left of our originally clicked on square
+                            if self.__game_square().get_row() < square.get_row():
+                                # Bottom left
+                                if 0 < self.__row + 2 < self.__board.get_size() - 1 or 0 < self.__col - 2 < self.__board.get_size() - 1:
+                                    new_bot_left = self.__board.get_game_square(self.__row + 2, self.__col - 2)
+                                    if new_bot_left.get_occupying_piece() is None:
+                                        list_of_candidate_game_squares.append(new_bot_left)
+                                        # TODO: Right now this only allows for one jump, need to check to see if
+                                        #  there are multi-jump moves available
+                            else:
+                                # Top Left
+                                if 0 < self.__row - 2 < self.__board.get_size() - 1 or 0 < self.__col - 2 < self.__board.get_size() - 1:
+                                    new_top_left = self.__board.get_game_square(self.__row - 2, self.__col - 2)
+                                    if new_top_left.get_occupying_piece() is None:
+                                        list_of_candidate_game_squares.append(new_top_left)
+                                        # TODO: Right now this only allows for one jump, need to check to see if
+                                        #  there are multi-jump moves available
 
+                        list_of_candidate_game_squares.remove(square)
 
-                top_left =  self.__board.get_game_square(self.__row-1, self.__col-1)
-                top_right = self.__board.get_game_square(self.__row-1, self.__col+1)
-                bot_left = self.__board.get_game_square(self.__row+1, self.__col-1)
-                bot_right = self.__board.get_game_square(self.__row+1. self.__col+1)
-
-        elif self.game_type == "chess":
-            #generate possible moves for a chess piece
-        else:
-            return -1
-        #TODO: implement this function
-        #following is for TESTING ONLY
-        if type(self.__Piece).__name__ == "King":
-	    #check surrounding squares 
-            #generate_king_moves(starting_location, is_castled) something like this -> in Pieces, returns list of tuples denoting possible moves without blocking considered
-            print("is king")
+            self.__moves = list_of_candidate_game_squares
             return 0
+
+        elif self.__game_type == "chess":
+            if type(self.__Piece).__name__ == "King":
+                # TODO: generate possible moves for King
+                pass
+            elif type(self.__Piece).__name__ == "Queen":
+                # TODO: generate possible moves for Queen
+                pass
+            elif type(self.__Piece).__name__ == "Bishop":
+                # TODO: generate possible moves for Bishop
+                pass
+            elif type(self.__Piece).__name__ == "Knight":
+                # TODO: generate possible moves for Knight
+                pass
+            elif type(self.__Piece).__name__ == "Rook":
+                # TODO: generate possible moves for Rook
+                pass
+            elif type(self.__Piece).__name__ == "Pawn":
+                # TODO: generate possible moves for Pawn
+                pass
         else:
             return -1
     
@@ -83,7 +139,7 @@ class PossibleMoves():
         """
         return: a list of moves that are legal
         """
-        return self.__move
+        return self.__moves
 
     def display_options(self):
         """
