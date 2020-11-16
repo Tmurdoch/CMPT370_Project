6d40d24826516dbbe2211ff28df3cc7e6996be85
Thomas Murdoch <thomasjmurd@gmail.com>
2020-11-16 16:54:47 -0600

    fixed pep8 for Game.py

--- Game.py
+++ Game.py
@@ -38,8 +38,9 @@ class Game:
         __dark_player: Player: The dark player object
         __board: Board: The game board
     """
+
     def __init__(self, game_type, colour_mode):
-        
+
         # I think based on PieceSet.py and the domain model
         # that this is going to be a string but i'm not
         # sure
@@ -54,7 +55,8 @@ class Game:
             # something went wrong here and it wasn't the users fault
             # so don't show an error, whatever tried to create a game
             # object will probably crash now
-            raise Exception("GameTypeErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
+            raise Exception(
+                "GameTypeErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
         # common for both chess and checkers
         if colour_mode >= len(COLOUR_STRING_LOOK_UP_TABLE):
             raise Exception("wrongColourOrSomethingFigureOutLater")
@@ -123,15 +125,16 @@ class Game:
         # write magic
         fp.write(MAGIC)
         game_mode = self.__game_type  # TODO: Isn't this a string? "chess" or "checkers"
-        ai_in_game = not ((self.__light_player.get_player_type()) and (self.__dark_player.get_player_type())) # good
+        ai_in_game = not ((self.__light_player.get_player_type()) and (
+            self.__dark_player.get_player_type()))  # good
         dark_player_is_ai = not (self.__dark_player.get_player_type())  # good
         dark_player_turn = self.__dark_player is self.__current_player  # good
         board_height = self.get_board().get_size()  # good
         board_width = board_height  # good
         colours = self.__colour_mode  # good
         timer_enabled = self.__dark_player.get_timer().get_enabled()  # good
-        light_player_time = self.__light_player.get_timer().get_time_remaining_s() # good
-        dark_player_time = self.__dark_player.get_timer().get_time_remaining_s() # good
+        light_player_time = self.__light_player.get_timer().get_time_remaining_s()  # good
+        dark_player_time = self.__dark_player.get_timer().get_time_remaining_s()  # good
         light_player_castled = self.__light_player.get_castled()
         dark_player_castled = self.__dark_player.get_castled()
         # write the header struct
@@ -144,7 +147,8 @@ class Game:
         while row != board_height:
             col = 0
             while col != board_width:
-                cur_piece = self.__board.get_game_square(row,col).get_occupying_piece()
+                cur_piece = self.__board.get_game_square(
+                    row, col).get_occupying_piece()
                 if cur_piece is None:
                     fp.write((0).to_bytes(1, byteorder="big"))
                     col += 1
@@ -202,9 +206,11 @@ class Game:
         fp = open(FILENAME, "rb")
         read_magic = fp.read(20)
         if read_magic != MAGIC:
-            raise Exception("ChessFileErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
+            raise Exception(
+                "ChessFileErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
 
-        file_version = int.from_bytes(fp.read(1), byteorder="big", signed=False)
+        file_version = int.from_bytes(
+            fp.read(1), byteorder="big", signed=False)
         if file_version == 0:
             # Do stuff for file version 0
             # if the file version changes
@@ -216,7 +222,8 @@ class Game:
             # check if file is at least big enough to hold the header
             if file_size < FILE_VER_ZERO_HEADER_SIZE:
                 fp.close()
-                raise Exception("ChessFileErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
+                raise Exception(
+                    "ChessFileErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
             fp.seek(20, SEEK_SET)
             f = fp.read(FILE_VER_ZERO_HEADER_SIZE-20)
             CURRENT_FILE_VERSION, game_mode, ai_in_game, dark_player_is_ai, dark_player_turn, board_height, \
@@ -254,8 +261,10 @@ class Game:
                                    Timer(dark_player_time, timer_enabled), dark_player_castled)
 
             # For now assume they are ideal piece sets
-            self.__light_player.build_piece_set(GAME_TYPE_STRING_LOOK_UP_TABLE[game_mode], COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][0])
-            self.__dark_player.build_piece_set(GAME_TYPE_STRING_LOOK_UP_TABLE[game_mode], COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][1])
+            self.__light_player.build_piece_set(
+                GAME_TYPE_STRING_LOOK_UP_TABLE[game_mode], COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][0])
+            self.__dark_player.build_piece_set(
+                GAME_TYPE_STRING_LOOK_UP_TABLE[game_mode], COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][1])
             # TODO: Right now we are just putting random chess pieces on the board, we need to put on pieces from the
             #  pieceset onto the board and then capture all the pieces that we didn't find on the board
 
@@ -284,21 +293,28 @@ class Game:
                     # decode object to char
                     if self.__game_type == GAME_TYPE_CHESS:
                         if chr(board_data[board_data_index]).lower() == "k":
-                            cur_square.put_piece_here(King(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                King(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                         elif chr(board_data[board_data_index]).lower() == "q":
-                            cur_square.put_piece_here(Queen(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                Queen(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                         elif chr(board_data[board_data_index]).lower() == "n":
-                            cur_square.put_piece_here(Knight(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                Knight(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                         elif chr(board_data[board_data_index]).lower() == "b":
-                            cur_square.put_piece_here(Bishop(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                Bishop(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                         elif chr(board_data[board_data_index]).lower() == "r":
-                            cur_square.put_piece_here(Rook(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                Rook(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                         elif chr(board_data[board_data_index]).lower() == "p":
                             # The pawn is an unmoved pawn
-                            cur_square.put_piece_here(Pawn(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                Pawn(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                         elif chr(board_data[board_data_index]).lower() == "q":
                             # The pawn is an moved pawn
-                            cur_square.put_piece_here(Pawn(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
+                            cur_square.put_piece_here(
+                                Pawn(COLOUR_STRING_LOOK_UP_TABLE[self.__colour_mode][is_dark]))
                             cur_square.get_occupying_piece().move()
                         else:
                             # unidentified piece, shouldn't be possible
@@ -307,12 +323,14 @@ class Game:
                     elif self.__game_type == GAME_TYPE_CHECKERS:
                         if is_dark:
                             cur_square.put_piece_here(
-                                self.__dark_player.get_piece_set().get_live_pieces()[found_dark_checkers_pieces]
+                                self.__dark_player.get_piece_set().get_live_pieces()[
+                                    found_dark_checkers_pieces]
                             )
                             found_dark_checkers_pieces += 1
                         else:
                             cur_square.put_piece_here(
-                                self.__light_player.get_piece_set().get_live_pieces()[found_light_checkers_pieces]
+                                self.__light_player.get_piece_set().get_live_pieces()[
+                                    found_light_checkers_pieces]
                             )
                             found_light_checkers_pieces += 1
 
@@ -330,7 +348,8 @@ class Game:
                 # Start at the back of the list of live pieces not placed on the board and capture them
                 for i in range(16-found_dark_checkers_pieces):
                     self.__dark_player.get_piece_set().capture_piece(
-                        self.__dark_player.get_piece_set().get_live_pieces()[15-i]
+                        self.__dark_player.get_piece_set().get_live_pieces()[
+                            15-i]
                     )
             elif self.__game_type == GAME_TYPE_CHESS:
                 pass
@@ -338,7 +357,8 @@ class Game:
         else:
             fp.close()
             # ui.showError("File version " + str(file_version) + " unsupported in this version, update the game")
-            raise Exception("ChessFileErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
+            raise Exception(
+                "ChessFileErrorOrSomethingFigureOutHowPeopleWantThisTOWOrk")
 
     def get_result(self):
         return
@@ -369,11 +389,11 @@ class Game:
 # if (__name__ == "__main__"):
 #    game_obj = Game("chess", Colours.Colour_Codes.RED_BLACK)
 #
-#    piece_obj = King("Red")	
-#        
+#    piece_obj = King("Red")
+#
 #    game_obj.get_board().get_game_square(0, 0).put_piece_here(self.__dark_player.get_piece_set().)
 #    game_obj.get_board().print_game_board()
-#    timer_obj = Timer(10, 20, 0) 
+#    timer_obj = Timer(10, 20, 0)
 #    game_obj.build_light_player("tom", PlayerType.human, timer_obj, 1)
 #    game_obj.build_dark_player("tom", PlayerType.human, timer_obj, 1)
 #    game_obj.save_to_file()
