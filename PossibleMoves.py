957d2e6f5292be4b0b3f30c02b3153bebebcc3d4
Michael Luciuk <mrl280@mail.usask.ca>
2020-11-22 12:21:06 -0600

    Added the parameters to the build_list_of_moves() docstring.

--- PossibleMoves.py
+++ PossibleMoves.py
@@ -9,9 +9,11 @@ GAME_TYPE_CHECKERS = 1
 
 def build_list_of_moves(input_game_square, input_game):
     """
-    For a given game square, build a list of all the legal game squares you can move to. Note: Even on success,
-    the list of possible moves might be an empty list.  An empty list means that piece has not legal moves :return:
-    :return list_of_candidate_game_squares: (GameSquare[]): List of destination squares you can legally move to
+    For a given game square, this function builds a list of all the legal game squares you can move to. Note: Even on
+    success, the list of possible moves might be an empty list.  An empty list means that piece has no legal moves.
+    :param input_game_square: GameSquare: The origin game square that you want to build a list of moves for.
+    :param input_game: The input game, required to get the game board and game type
+    :return list_of_candidate_game_squares: GameSquare[]: List of destination squares you can legally move to
     """
     input_piece = input_game_square.get_occupying_piece()
     input_row = input_game_square.get_row()
@@ -715,8 +717,8 @@ def build_list_of_moves(input_game_square, input_game):
 
             if input_row - 1 >= 0 and 0 <= input_col <= input_board.get_size() - 1:
 
-                # left most case (edge case for columns 0)
                 if input_col == 0:
+                    # left most case (edge case for column 0)
                     if input_board.get_game_square(input_row - 1,
                                                    input_col + 1).get_occupying_piece() is not None:
                         if input_board.get_game_square(input_row - 1, input_col + 1).get_occupying_piece() \
@@ -725,8 +727,8 @@ def build_list_of_moves(input_game_square, input_game):
                             list_of_candidate_game_squares.append(input_board.get_game_square(input_row - 1,
                                                                                               input_col + 1))
 
-                # right most case (edge case for columns 7)
                 elif input_col == input_board.get_size() - 1:
+                    # right most case (edge case for column 7)
                     if input_board.get_game_square(input_row - 1,
                                                    input_col - 1).get_occupying_piece() is not None:
                         if input_board.get_game_square(input_row - 1, input_col - 1).get_occupying_piece() \
@@ -735,10 +737,10 @@ def build_list_of_moves(input_game_square, input_game):
                             list_of_candidate_game_squares.append(input_board.get_game_square(input_row - 1,
                                                                                               input_col - 1))
 
-                # Now we are working with middle pawns. Checks for both front diagonals and
-                # if there is a non friendly add it to the list
                 else:
-                    # front left
+                    # Now we are working with middle pawns. Checks for both front diagonals and
+                    # if there is a non friendly add it to the list.
+                    # First check the front left
                     if input_board.get_game_square(input_row - 1,
                                                    input_col - 1).get_occupying_piece() is not None:
                         if input_board.get_game_square(input_row - 1, input_col - 1).get_occupying_piece() \
@@ -754,7 +756,7 @@ def build_list_of_moves(input_game_square, input_game):
                             # Capture move, add it to the list
                             list_of_candidate_game_squares.append(input_board.get_game_square(input_row - 1,
                                                                                               input_col - 1))
-                # TODO: Look for "en passant" pawn capture opportunity
+                # TODO: Look for "en passant" pawn capture opportunity.
 
         else:
             # Could not identify the type of piece
@@ -769,7 +771,7 @@ def build_list_of_moves(input_game_square, input_game):
 
 def checkers_jump(input_board, input_piece, input_game_square, list_moves):
     """
-    Recursive function to identify any checkers jumps
+    Iterative function to identify any checkers jumps
     Used to help build the list of checkers jump moves
     Does not return anything, just keeps appending to the list of candidate moves
     :param input_piece: Piece: The original piece we are moving, doesn't change as we recurse
@@ -818,7 +820,7 @@ def checkers_jump(input_board, input_piece, input_game_square, list_moves):
                                                               input_game_square.get_col() + 2), list_moves)
 
     if input_piece.get_promotion_status():
-        # The coin is promoted and can also move backwards
+        # The coin is promoted and can also jump backwards.
         # TODO: If the coin is promoted, we need to also check for the backwards jumps.
         #  Can't just add on the same logic used for forward jumps or it will result in an infinite loop.
         pass
