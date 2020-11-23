1d0dd4c0e3e56d06623abf459261797c38d05353
Thomas Murdoch <thomasjmurd@gmail.com>
2020-11-23 14:36:05 -0600

    added game obejct parameter to Board window, should be set up earlier though mouse_press_event prints game locations but not correctly, create_location_list is an actual 2d array now

--- UIMenus.py
+++ UIMenus.py
@@ -107,7 +107,10 @@ class TheWindow(Gtk.Window):
         def customization_start_clicked(self, button):
                 print("This should go to Board Window")
                 #board = BoardWindow(self.__game, self.__game_type)
-                board = BoardWindow("Test", "multiplayer")
+                temp_game = Game(1, 0)
+                #TODO: the game should be setup way earlier in the UI, this is jsut a placeholder
+                #TODO: MOVE THIS WHEN THE OTHER UI WINDOWS ARE FUNCTIONAL
+                board = BoardWindow("Test", "multiplayer", temp_game)
                 board.show_all()
 
 
@@ -268,9 +271,13 @@ class CustomizationGrid(Gtk.Grid):
 
 
 class BoardWindow(Gtk.Window):
-    def __init__(self, game, game_type):
+    def __init__(self, game, game_type, game_obj):
+        """
+        @param game_obj: actual game object, initialize by Game()
+        """
         Gtk.Window.__init__(self, title=game + " " + game_type)
         self.__game = game
+        self.__game_obj = game_obj
         self.surface = None
 
         self.set_resizable(False)
@@ -380,31 +387,39 @@ class BoardWindow(Gtk.Window):
 
         if event.button == 1:
             self.mouse_pointer(checkerboard_area, event.x, event.y)
-            list_list = create_location_list(self.__game.get_board().get_size())
-            y=1
-            for x in list_list:
-                if event.x <= x[0] and event.y <= x[1]:
-                    print(event.x, event.y)
-                    print("square ", y)
-                    y+=1
+            size = self.__game_obj.get_board().get_size()
+            list_list = self.create_location_list(size)
+            
+            game_col = 0
+            for col in list_list:
+                game_row = 0
+                for row in col:
+                    if event.x <= row[0] and event.y <= row[1]:
+                        #print(event.x, event.y)
+                        print("grid location: ", game_col, game_row)
+                    game_row+=1
+                game_col += 1    
 
         return True
 
-    def create_location_list(size):
+    def create_location_list(self, size):
         """
         creates a 2d list of size n where each i in the list is [x, y] and 
         denotes a location to be placed on the UI window
         @return: 2d list of integers
         """
-        cur_width = 50 #start at 50
         cur_length = 50
         rv_list = []
 
         for i in range(size):
+            col_list = []
+            cur_width = 50
             for j in range(size):
-                rv_list.append([cur_width, cur_length])
+                col_list.append([cur_width, cur_length])
                 cur_width += 50
             cur_length += 50
+            rv_list.append(col_list)
+    
         return rv_list
 
     def displayclock(self):
