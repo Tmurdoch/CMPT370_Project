e7933680aff26746847d65ede7452f80223ea05d
Michael Luciuk <mrl280@mail.usask.ca>
2020-11-26 16:37:54 -0600

    Updated timer docstrings to be consistent with the rest of the system.

--- Timer.py
+++ Timer.py
@@ -8,32 +8,31 @@ import time
 class Timer(object):
     """
     Timer is an object that keeps and updates the time remaining for a Player.
-    Time is in nanoseconds (integer)
+    Time is in nanoseconds (integer).
 
     Attributes:
-        __time_remaining_ns: Int: The remaining time on the timer in nanoseconds
-        __enabled: Bool: Whether or not the timer is enabled
-        __running: Bool: Whether or not timer is running
-        __timeOnStart: Int: The time as of when the timer was last started
+        __time_remaining_ns: Int: The remaining time on the timer in nanoseconds.
+        __enabled: Bool: Whether or not the timer is enabled.
+        __running: Bool: Whether or not timer is running.
+        __timeOnStart: Int: The time as of when the timer was last started.
     """
 
     def __init__(self, allotted_time, enabled):
         """
         Initializes a Timer object.
-        :param: allotted_time: The initial time given to a Player to make their moves.
-            Remaining time if loading from a file.  Takes in time in seconds.
-        :param: enabled: Bool that determines if the timer is enabled or not (True for enabled, False otherwise)
+        :param allotted_time: int: TIME IN SECONDS! The initial time given to a Player to make their moves.
+            Remaining time if loading from a file.
+        :param enabled: Bool: Boolean that determines if the timer is enabled or not (True for enabled, False otherwise)
         """
-        self.__time_remaining_ns = allotted_time * \
-            (10 ** 9)  # Can't be manually changed, no setter
-        self.__enabled = enabled  # is this for starting the timer?
+        self.__time_remaining_ns = allotted_time * (10 ** 9)  # Can't be manually changed, no setter
+        self.__enabled = enabled
         self.__running = False
         self.__timeOnStart = None
 
     def timed_out(self):
         """
-        Used to check if the player has timed out
-        :return: bool: True if the player is out of time, False otherwise
+        Used to check if the player has timed out.
+        :return: Bool: True if the player is out of time, False otherwise.
         """
         if self.__enabled:
             return self.__time_remaining_ns < 0
@@ -42,8 +41,8 @@ class Timer(object):
 
     def little_time_left(self):
         """
-        Used to check if the player has little time left
-        :return: bool: True if the player has little time left, False otherwise
+        Used to check if the player has little time left.
+        :return: bool: True if the player has little time left, False otherwise.
         """
         if self.__enabled:
             return self.__time_remaining_ns < (60 * (10 ** 9))  # 1 minute
@@ -51,7 +50,7 @@ class Timer(object):
             return False
 
     def start(self):
-        """Starts the timer"""
+        """ Starts the timer """
         if self.__enabled and not self.__running:
             self.__running = True
             self.__timeOnStart = time.time_ns()
@@ -60,7 +59,7 @@ class Timer(object):
             return False
 
     def stop(self):
-        """Stops the timer and update the time remaining"""
+        """ Stops the timer and updates the time remaining """
         if self.__enabled and self.__running:
             self.__time_remaining_ns = self.get_time_remaining_ns()
             self.__running = False
@@ -69,7 +68,7 @@ class Timer(object):
             return False
 
     def get_time_remaining_ns(self):
-        """:returns: the time remaining for a Player in nanoseconds"""
+        """ :return: float: the time remaining for a Player in nanoseconds """
         if self.__enabled:
             if self.__running:
                 return self.__time_remaining_ns - (time.time_ns() - self.__timeOnStart)
@@ -79,7 +78,7 @@ class Timer(object):
             return False
 
     def get_time_remaining_s(self):
-        """:returns: the time remaining for a Player in nanoseconds"""
+        """ :return: float: the time remaining for a Player in seconds """
         if self.__enabled:
             if self.__running:
                 return (self.__time_remaining_ns - (time.time_ns() - self.__timeOnStart)) / (10 ** 9)
@@ -89,5 +88,5 @@ class Timer(object):
             return False
 
     def get_enabled(self):
-        """:returns: True if the timer is enabled, False otherwise"""
+        """ :return: bool: True if the timer is enabled, False otherwise """
         return self.__enabled
