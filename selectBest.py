6da6be06a0102a6f2e7b8b0be2a4682c130d5fea
Michael Luciuk <mrl280@mail.usask.ca>
2020-11-26 16:34:17 -0600

    Fixing up docstring in select best so it is consistent with the rest of the system.

--- selectBest.py
+++ selectBest.py
@@ -7,9 +7,9 @@ import random
 
 def select_best(candidate_game_squares):
     """
-    Chooses and return the best game square to move to from a list of candidate squares
-    :param: List of GameSquares to choose from
-    :return: GameSquare, the best game square to move to. Returns None if there are no moves for that square
+    Chooses and returns the best game square to move to from a list of candidate squares.
+    :param: GameSquare[]: List of GameSquares to choose from.
+    :return: GameSquare: The best game square to move to. Returns None if there are no moves for that square.
     """
     if not candidate_game_squares:
         # List of moves is empty
@@ -18,7 +18,7 @@ def select_best(candidate_game_squares):
         # The is only one move, it has to be the best
         return candidate_game_squares[0]
     else:
-        # TODO: Some AI code to evaluate the list of moves to choose the best one, in the mean time we are jsut
+        # TODO: Some AI code to evaluate the list of moves to choose the best one, in the mean time we are just
         #  returning the first one in the list
         # Right now we will just return a random candidate
         random_index = (random.randrange(0, len(candidate_game_squares), 1))
