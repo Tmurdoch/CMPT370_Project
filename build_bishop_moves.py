f395ecaa9b08daa6c77c07cce037b97a914b1624
Michael Luciuk <mrl280@mail.usask.ca>
2020-11-27 14:26:14 -0600

    An initial attempt at factoring out duplicate code fragments in build_bishop_moves.py using the rook logic helper

--- build_bishop_moves.py
+++ build_bishop_moves.py
@@ -2,6 +2,8 @@
 # CMPT 370 Group 4, Fall 2020
 # Authors: Antoni Jann Palazo, Brian Denton, Joel Berryere,
 # Michael Luciuk, Thomas Murdoch
+from build_rook_moves import rook_move_adder
+from chess_move_adder import chess_move_adder
 
 
 def build_bishop_moves(input_game_square, input_game):
@@ -16,7 +18,7 @@ def build_bishop_moves(input_game_square, input_game):
     input_row = input_game_square.get_row()
     input_col = input_game_square.get_col()
     input_board = input_game.get_board()
-    list_of_candidate_game_squares = []
+    candidate_game_squares = []
 
     # The bishop can be moved any number of unoccupied squares in a straight line diagonally.
     # Stop if we see a piece. If piece is unfriendly add the capture move to the list. Here are the step taken:
@@ -28,119 +30,60 @@ def build_bishop_moves(input_game_square, input_game):
     #   6. if empty then add it to list and keep going
 
     # Top Right
-    row_pos = input_row
-    col_pos = input_col
-
-    stopper = False
-    if row_pos != 0:
-        for row in range(row_pos - 1, -1, -1):
-            if col_pos != input_board.get_size() - 1:
-                if not stopper:
-                    for col in range(col_pos + 1, input_board.get_size()):
-                        if abs(row_pos - row) == abs(col_pos - col):
-                            if input_board.get_game_board()[row][col].get_occupying_piece() is not None:
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() == input_piece.get_colour():
-                                    stopper = True
-                                    break
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() != input_piece.get_colour():
-                                    list_of_candidate_game_squares.append(
-                                        input_board.get_game_board()[row][col])
-                                    stopper = True
-                                    break
-                            else:
-                                # Square was empty, add it to the list
-                                list_of_candidate_game_squares.append(
-                                    input_board.get_game_board()[row][col])
-                else:
-                    break
+    if input_row > 0:
+        for row in range(input_row - 1, -1, -1):
+            # If we are not in the last row, then loop up through the remaining rows
+            if input_col < input_board.get_size() - 1:
+                for col in range(input_col + 1, input_board.get_size()):
+                    # if we are not in the last col, then loop up through all the remaining columns
+                    if abs(input_row - row) == abs(input_col - col):
+                        # Confirm we are on the diagonal and then check the square
+                        if not rook_move_adder(input_board, candidate_game_squares, input_piece.get_colour(), row, col):
+                            break
+            else:
+                break
 
     # Top Left
-    row_pos = input_row
-    col_neg = input_col
 
-    stopper = False
-    if row_pos != 0:
-        for row in range(row_pos - 1, -1, -1):
-            if col_neg != 0:
-                if not stopper:
-                    for col in range(col_neg - 1, -1, -1):
-                        if abs(row_pos - row) == abs(col_neg - col):
-                            if input_board.get_game_board()[row][col].get_occupying_piece() is not None:
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() == input_piece.get_colour():
-                                    stopper = True
-                                    break
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() != input_piece.get_colour():
-                                    list_of_candidate_game_squares.append(
-                                        input_board.get_game_board()[row][col])
-                                    stopper = True
-                                    break
-                            else:
-                                # Square was empty, add it to the list
-                                list_of_candidate_game_squares.append(
-                                    input_board.get_game_board()[row][col])
-                else:
-                    break
+    if input_row > 0:
+        for row in range(input_row - 1, -1, -1):
+            # If we are not in the last row, then loop up through the remaining rows
+            if input_col > 0:
+                for col in range(input_col - 1, -1, -1):
+                    # if we are not in the last col, then loop up through all the remaining columns
+                    if abs(input_row - row) == abs(input_col - col):
+                        # Confirm we are on the diagonal and then check the square
+                        if not rook_move_adder(input_board, candidate_game_squares, input_piece.get_colour(), row, col):
+                            break
+            else:
+                break
 
     # Bottom Right
-    row_neg = input_row
-    col_pos = input_col
-
-    stopper = False
-    if row_neg != input_board.get_size() - 1:
-        for row in range(row_neg + 1, input_board.get_size()):
-            if col_pos != input_board.get_size() - 1:
-                if not stopper:
-                    for col in range(col_pos + 1, input_board.get_size()):
-                        if abs(row_neg - row) == abs(col_pos - col):
-                            if input_board.get_game_board()[row][col].get_occupying_piece() is not None:
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() == input_piece.get_colour():
-                                    stopper = True
-                                    break
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() != input_piece.get_colour():
-                                    list_of_candidate_game_squares.append(
-                                        input_board.get_game_board()[row][col])
-                                    stopper = True
-                                    break
-                            else:
-                                # Square was empty, add it to the list
-                                list_of_candidate_game_squares.append(
-                                    input_board.get_game_board()[row][col])
-                else:
-                    break
+    if input_row < input_board.get_size() - 1:
+        for row in range(input_row + 1, input_board.get_size()):
+            # If we are not in the last row, then loop up through the remaining rows
+            if input_col < input_board.get_size() - 1:
+                # if we are not in the last col, then loop up through all the remaining columns
+                for col in range(input_col + 1, input_board.get_size()):
+                    if abs(input_row - row) == abs(input_col - col):
+                        # Confirm we are on the diagonal and then check the square
+                        if not rook_move_adder(input_board, candidate_game_squares, input_piece.get_colour(), row, col):
+                            break
+            else:
+                break
 
     # Bottom Left
-    row_neg = input_row
-    col_neg = input_col
+    if input_row < input_board.get_size():
+        for row in range(input_row + 1, input_board.get_size()):
+            # If we are not in the last row, then loop up through the remaining rows
+            if input_col > 0:
+                for col in range(input_col - 1, -1, -1):
+                    # if we are not in the last col, then loop up through all the remaining columns
+                    if abs(input_col - row) == abs(input_col - col):
+                        # Confirm we are on the diagonal and then check the square
+                        if not rook_move_adder(input_board, candidate_game_squares, input_piece.get_colour(), row, col):
+                            break
+            else:
+                break
 
-    stopper = False
-    if row_neg != input_board.get_size():
-        for row in range(row_neg + 1, input_board.get_size()):
-            if col_neg != 0:
-                if not stopper:
-                    for col in range(col_neg - 1, -1, -1):
-                        if abs(row_neg - row) == abs(col_neg - col):
-                            if input_board.get_game_board()[row][col].get_occupying_piece() is not None:
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() == input_piece.get_colour():
-                                    stopper = True
-                                    break
-                                if input_board.get_game_board()[row][col].get_occupying_piece() \
-                                        .get_colour() != input_piece.get_colour():
-                                    list_of_candidate_game_squares.append(
-                                        input_board.get_game_board()[row][col])
-                                    stopper = True
-                                    break
-                            else:
-                                # Square was empty, add it to the list
-                                list_of_candidate_game_squares.append(
-                                    input_board.get_game_board()[row][col])
-                else:
-                    break
-
-    return list_of_candidate_game_squares
+    return candidate_game_squares
