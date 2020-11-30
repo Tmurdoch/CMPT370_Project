ef37217f6964e44c5565beff7b6b1a46a951627b
Joel Berryere (jtb648) <jtb648@mail.usask.ca>
2020-11-30 16:31:52 -0600

    Added docstrings for all UI menus except for BoardGrid and TheWindow since Brian might be better suited to explain how those work. Also changed some variables to be self. to follow our other classes.

--- UIGameChoiceBox.py
+++ UIGameChoiceBox.py
@@ -10,9 +10,16 @@ from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, Rsvg, GLib
 
 
 class GameChoiceBox(Gtk.Box):
+    """
+       The GameChoiceBox initializes a Gtk Box to hold Gtk buttons that allow a user to choose what game they want to
+       play.
+       Attributes:
+           chess_button: A Gtk button with a label "Chess" that sets the game to Chess.
+           checkers_button: A Gtk button with a label "Checkers" that sets the game to Checkers.
+           back_button: A Gtk button that allows the user to go back to the previous menu.
+    """
     def __init__(self):
-        Gtk.Box.__init__(
-            self, orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL, spacing=10)
 
         self.chess_button = Gtk.Button.new_with_label("Chess")
         self.chess_button.set_property("width-request", 300)
--- UIPlayerTypeBox.py
+++ UIPlayerTypeBox.py
@@ -10,9 +10,17 @@ from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, Rsvg, GLib
 
 
 class PlayerTypeBox(Gtk.Box):
+    """
+       The CustomizationGrid initializes a Gtk Box to hold Gtk buttons that allow a user to choose the type of opponent
+       they want to play against with the choices being single player against an AI or multi player against a local
+       Human.
+       Attributes:
+           single_button: A Gtk button with the label "Single Player" that sets the game type to Single Player.
+           multi_player_button: A Gtk button with the label "Multi Player" that sets the game type to Multi Player.
+           back_button: A Gtk button that allows the user to go back to the previous menu.
+    """
     def __init__(self):
-        Gtk.Box.__init__(
-            self, orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL, spacing=10)
 
         self.single_button = Gtk.Button.new_with_label("Single-Player")
         # single_button.get_style_context().add_class("suggested-action") changes button to blue
@@ -20,7 +28,7 @@ class PlayerTypeBox(Gtk.Box):
         self.single_button.set_property("height-request", 100)
         self.pack_start(self.single_button, True, True, 0)
 
-        self.multiplayer_button = Gtk.Button.new_with_label("Multi-Player")
+        self.multi_player_button = Gtk.Button.new_with_label("Multi-Player")
         self.multiplayer_button.set_property("width-request", 300)
         self.multiplayer_button.set_property("height-request", 100)
         self.pack_start(self.multiplayer_button, True, True, 0)
